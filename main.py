5b5ed37f966cfccbbbf243cf6726998b9675e4e9
Mateus-Arauj <mateus9509195091@gmail.com>
2023-06-28 20:24:16 -0300

    Adicionando a leitura do array do csv

--- main.py
+++ main.py
@@ -1,15 +1,12 @@
 # Dados de entrada
 import numpy as np
 from perceptron import Perceptron
+import pandas as pd
 
-X = np.array([
-    [5.1, 3.5, 1.4, 0.2],
-    [4.9, 3.0, 1.4, 0.2],
-    [4.7, 3.2, 1.3, 0.2],
-    [7.0, 3.2, 4.7, 1.4],
-    [6.4, 3.2, 4.5, 1.5],
-    [6.9, 3.1, 4.9, 1.5]
-])
+
+df = pd.read_csv('dataset.csv')
+df = df.iloc[:, :4]
+X = df.values
 # Rótulos correspondentes
 y = np.array([1, 1, 1, -1, -1, -1])
 
